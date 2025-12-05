aad9f7f4fe2cedb1beaab545f054e173f4625d0f
Nurussyifaa <nurussyifaa3@gmail.com>
2025-12-05 15:40:42 +0800

    nambah route tes

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => redirect()->route('posts.index'));
+Route::get('/', fn() => redirect()->route('posts.index'));
 Route::resource('posts', \App\Http\Controllers\PostController::class);
+
+Route::get('tes', function () {
+    return 'tes';
+});
