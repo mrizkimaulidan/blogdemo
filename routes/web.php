d5de7897df2c502808ea1911531dd0554949d1f8
mrizkimaulidan <mrizkimaulidan@icloud.com>
2025-12-05 15:44:25 +0800

    ngubah

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,5 @@ Route::get('/', fn() => redirect()->route('posts.index'));
 Route::resource('posts', \App\Http\Controllers\PostController::class);
 
 Route::get('tes', function () {
-    return 'tes';
+    return 'kiki';
 });
